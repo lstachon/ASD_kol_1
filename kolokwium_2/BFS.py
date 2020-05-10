bf84a15b4fbf1096d32cfa6b6ea033fa507d83e0
lstachon <lukaszstachon13@gmail.com>
2020-05-10 15:34:48 +0200

    Add structure.

--- kolokwium_2/BFS.py
+++ kolokwium_2/BFS.py
@@ -1,2 +1,19 @@
+#bfs dla macierzy sasiedztwa
+
+def BFS(G,s):
+    visited = [0 for i in range(len(G))]
+    visited[s] = 1
+    queue =[]
+    queue.append(s)
+    r = []
+    while len(queue)!= 0:
+        a = queue.pop(0)
+        print(a," ")
+        for i in range(len(G[a])):
+
+            if(G[a][i]==1):
+                if visited[i] == 0:
+                    queue.append(i)
+                    visited[i]=1
 
 
--- kolokwium_2/najkrotsze_sciezki/algorytm_sciezki_bfs_dla_malych_wag.py
+++ kolokwium_2/najkrotsze_sciezki/algorytm_sciezki_bfs_dla_malych_wag.py
@@ -1,30 +1,34 @@
 #algorytm znajdujący sciezke dla malych wag
 
-def BFS(G,s):
+def BFS(G,s,e):
     visited = [0 for i in range(len(G))]
     visited[s] = 1
     queue =[]
     queue.append(s)
-    r = []
+    result =[]
     while len(queue)!= 0:
         a = queue.pop(0)
         print(a," ")
+        if(a==e):
+            return 0
+
         for i in range(len(G[a])):
 
-            if(G[a][i]==1):
+            if(G[a][i]>0):
                 if visited[i] == 0:
                     queue.append(i)
-                    visited[i]=1
+                    G[a][i]-=1
+                    if(G[a][i]==0):
+                        visited[i]=1
+
 
 
 
 G=[[0 for i in range(4)] for j in range(4)]
 
-G[0][1]=1
-G[0][2]=1
-G[1][2]=1
-G[2][0]=1
-G[2][3]=1
-G[3][3]=1
+G[0][1]=5
+G[0][2]=2
+G[2][1]=1
+G[1][3]=1
 
-BFS(G,2)
+BFS(G,0,3)
